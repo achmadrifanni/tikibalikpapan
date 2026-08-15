ddaf0534539b06841a92d64fca0302c1824140e9
achmadrifanni <achmadrifanni.ar@gmail.com>
2026-08-15 16:48:16 +0800

    Refactor header and navigation structure for improved functionality and styling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+const navToggle = document.querySelector(".nav__toggle");
+const navbar = document.querySelector(".navbar");
+
+navToggle.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+const closeMenu = () => {
+  navbar.classList.remove("active");
+};
+
+document.addEventListener("click", (event) => {
+  const clickedInsideNav = navbar.contains(event.target);
+  const clickedHamburger = navToggle.contains(event.target);
+
+  if (!clickedInsideNav && !clickedHamburger) {
+    closeMenu();
+  }
+});
+
 const termModal = document.getElementById("termModal");
 const termBtnOpen = document.getElementById("termBtnOpen");
 const termBtnClose = document.getElementById("termBtnClose");
