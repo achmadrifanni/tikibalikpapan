bd07cbf46d40a09e0be5a91042cabf1268121f07
achmadrifanni <achmadrifanni.ar@gmail.com>
2026-08-23 15:39:57 +0800

    Fix table body ID in gerai.js and gerai.html for correct data binding

--- js/gerai.js
+++ js/gerai.js
@@ -1,6 +1,6 @@
 import { data } from "./data-gerai.js";
 
-const tableBody = document.querySelector("#data-table-body");
+const tableBody = document.querySelector("#geraiData");
 
 data.forEach((item) => {
   const row = document.createElement("tr");
